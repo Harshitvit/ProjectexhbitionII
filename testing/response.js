c3724cbb97e748057226c72643890686805f7f54
Harshitvit <114172949+Harshitvit@users.noreply.github.com>
2023-02-14 08:27:36 +0530

    Add files via upload

---
+++ testing/response.js
@@ -0,0 +1,9 @@
+const responseObj = {
+  hello: "Hey ! How are you doing ?",
+  hey: "Hey! What's Up",
+  hi: "Hey! What's Up",
+  wassup: "im good wbu?",
+  namesta: "sasrikyal ji",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+};
